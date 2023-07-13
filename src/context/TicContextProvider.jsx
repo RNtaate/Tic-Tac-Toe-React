51f1f34ba88743b87704abfc6d8ec8b914f12092
RNtaate <royhyde14@gmail.com>
2023-07-13 22:12:37 +0300

    Create the initial states in the Context provider
    Import and use it in the main page.

--- src/context/TicContextProvider.jsx
+++ src/context/TicContextProvider.jsx
@@ -3,10 +3,36 @@ import React, { useState, createContext } from 'react';
 export const TicContext = createContext();
 
 const TicContextProvider = ({ children }) => {
-  const [original, setOriginal] = useState('Roy');
+  const players = {
+    player1: 'ROY',
+    player2: 'SHARON',
+  };
+
+  const playerSymbols = {
+    player1: 'X',
+    player2: 'O',
+  };
+
+  const [player1Moves, setPlayer1Moves] = useState([]);
+  const [player2Moves, setPlayer2Moves] = useState([]);
+  const [combinedMoves, setCombinedMoves] = useState([]);
+  const [currentMove, setCurrentMove] = useState('');
+  const [player1Turn, setPlayer1Turn] = useState(true);
+  const [player1Win, setPlayer1Win] = useState(true);
+  const [player2Win, setPlayer2Win] = useState(false);
+  const [draw, setDraw] = useState(false);
 
   const contextValue = {
-    original,
+    players,
+    playerSymbols,
+    player1Moves,
+    player2Moves,
+    combinedMoves,
+    currentMove,
+    player1Turn,
+    player1Win,
+    player2Win,
+    draw,
   };
 
   return (
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { TicContext } from '../context/TicContextProvider';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  const context = useContext(TicContext);
+  const { players } = context;
+
+  return (
+    <div>
+      <h3>PLAYER ONE : {players.player1} </h3>
+      <h3>PLAYER TWO : {players.player2} </h3>
+    </div>
+  );
 };
 
 export default MainPage;
