9ec74bd2a0813ba76f1b29da8fcd8981c6519b42
RNtaate <royhyde14@gmail.com>
2023-07-13 22:50:51 +0300

    Update the design of the WinDraw component
    Create playerResultReturn function

--- src/components/WinDraw.jsx
+++ src/components/WinDraw.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+import { TicContext } from '../context/TicContextProvider';
 
 const WinDraw = () => {
-  return <div>WinDraw</div>;
-};
+  const context = useContext(TicContext);
+  const { players, player1Win, player2Win, draw } = context;
 
+  const playerResultReturn = (player) => {
+    let playerName = players[player.toString()];
+    return (
+      <div>
+        <p>{playerName} WINS 🎉🎉🎉 </p>
+      </div>
+    );
+  };
+
+  if (player1Win) {
+    return playerResultReturn('player1');
+  } else if (player2Win) {
+    return playerResultReturn('player2');
+  }
+
+  return (
+    <div>
+      <p>IT IS A DRAW!</p>
+    </div>
+  );
+};
 export default WinDraw;
