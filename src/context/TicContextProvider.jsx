8402b611f897f4ef87a1243f78630fe1298554f2
RNtaate <royhyde14@gmail.com>
2023-07-14 00:07:50 +0300

    Create initialStateValues object
    Import and use the initialStateValues in the project context.

--- src/context/TicContextProvider.jsx
+++ src/context/TicContextProvider.jsx
@@ -1,5 +1,7 @@
 import React, { useState, createContext } from 'react';
 
+import initialStateValues from '../services/InitialStateValues';
+
 export const TicContext = createContext();
 
 const TicContextProvider = ({ children }) => {
@@ -13,14 +15,22 @@ const TicContextProvider = ({ children }) => {
     player2: 'O',
   };
 
-  const [player1Moves, setPlayer1Moves] = useState([]);
-  const [player2Moves, setPlayer2Moves] = useState([]);
-  const [combinedMoves, setCombinedMoves] = useState([]);
-  const [currentMove, setCurrentMove] = useState('');
+  const [player1Moves, setPlayer1Moves] = useState(
+    initialStateValues.player1Moves
+  );
+  const [player2Moves, setPlayer2Moves] = useState(
+    initialStateValues.player2Moves
+  );
+  const [combinedMoves, setCombinedMoves] = useState(
+    initialStateValues.combinedMoves
+  );
+  const [currentMove, setCurrentMove] = useState(
+    initialStateValues.currentMove
+  );
   const [player1Turn, setPlayer1Turn] = useState(true);
   const [player1Win, setPlayer1Win] = useState(true);
-  const [player2Win, setPlayer2Win] = useState(false);
-  const [draw, setDraw] = useState(false);
+  const [player2Win, setPlayer2Win] = useState(initialStateValues.player2Win);
+  const [draw, setDraw] = useState(initialStateValues.draw);
 
   const contextValue = {
     players,
---
+++ src/services/InitialStateValues.js
@@ -0,0 +1,12 @@
+const initialStateValues = {
+  player1Moves: [],
+  player2Moves: [],
+  combinedMoves: [],
+  currentMove: '',
+  player1Turn: false,
+  player1Win: false,
+  player2Win: false,
+  draw: false,
+};
+
+export default initialStateValues;
