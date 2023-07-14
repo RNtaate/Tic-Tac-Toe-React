6124dd78edc988b9fb409589f63c9e80fbba8cf2
RNtaate <royhyde14@gmail.com>
2023-07-14 15:00:15 +0300

    Create the makeSelection function to implement playing a choice for the game
    Update and Fix the Symbol Component.
    
    Add click evenet to the div for making a choice.

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -4,9 +4,22 @@ import { TicContext } from '../context/TicContextProvider';
 
 const Tile = ({ tileNumber }) => {
   const context = useContext(TicContext);
-  const { combinedMoves, playerSymbols } = context;
+  const { combinedMoves, playerSymbols, setCombinedMoves, player1Turn } =
+    context;
 
-  const SymbolComponent = (player) => {
+  const makeSelection = (e) => {
+    e.stopPropagation();
+    console.log('This is the div that you clicked with id: ', e.target.id);
+    setCombinedMoves((prev) => {
+      return {
+        ...prev,
+        [e.target.id.toString()]: player1Turn ? 'player1' : 'player2',
+      };
+    });
+  };
+
+  const SymbolComponent = ({ player }) => {
+    console.log('You have reached the symbol component with player: ', player);
     return <div>{playerSymbols[player]}</div>;
   };
 
@@ -15,7 +28,9 @@ const Tile = ({ tileNumber }) => {
       {combinedMoves[tileNumber.toString()] ? (
         <SymbolComponent player={combinedMoves[tileNumber.toString()]} />
       ) : (
-        <div>{tileNumber} unchosen tile</div>
+        <div id={tileNumber} onClick={makeSelection}>
+          {tileNumber} unchosen tile
+        </div>
       )}
     </div>
   );
