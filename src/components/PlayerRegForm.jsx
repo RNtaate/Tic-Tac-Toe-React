9a21dd1c018bde71f4b7502411e0222ca78c88ff
RNtaate <royhyde14@gmail.com>
2023-07-20 17:33:20 +0300

    Add functinoality to bringup and hide the PlayerRegForm in the Welcom page.

--- src/components/PlayerRegForm.jsx
+++ src/components/PlayerRegForm.jsx
@@ -3,18 +3,21 @@ import React from 'react';
 import mainPageStyles from '../styles/MainPage.module.css';
 import styles from '../styles/PlayerRegForm.module.css';
 
-const PlayerRegForm = () => {
+const PlayerRegForm = ({ setShowForm }) => {
   return (
     <section
       className={`${mainPageStyles.resultSection} ${styles.formBackdropSection} d-flex justify-content-center align-items-center`}
+      onClick={(e) => setShowForm(false)}
     >
-      <div className="formContainerDiv">
+      <div className="formContainerDiv" onClick={(e) => e.stopPropagation()}>
         <h3>Please enter the players' names here</h3>
         <form action="#">
           <input type="text" placeholder="Player 1's name" name="player1" />
           <input type="text" placeholder="Player 2's name" name="player2" />
           <button type="submit">Submit</button>
-          <button type="button">Cancel</button>
+          <button type="button" onClick={() => setShowForm(false)}>
+            Cancel
+          </button>
         </form>
       </div>
     </section>
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from '../styles/Welcome.module.css';
 import PlayerRegForm from '../components/PlayerRegForm';
 
 const Welcome = () => {
+  const [showForm, setShowForm] = useState(false);
   return (
     <section
       className={`${styles.welcomeMainSection} d-flex justify-content-center align-items-center`}
     >
-      <PlayerRegForm />
+      {showForm && <PlayerRegForm setShowForm={setShowForm} />}
       <div
         className={`${styles.welcomeContainerDiv} d-flex justify-content-center align-items-center flex-col`}
       >
         <h1 className={styles.welcomeHeading}>WELCOME TO NORP'S TIC-TAC-TOE</h1>
-        <button type="button" className={styles.welcomeStartButton}>
+        <button
+          type="button"
+          className={styles.welcomeStartButton}
+          onClick={() => setShowForm(true)}
+        >
           START
         </button>
       </div>
