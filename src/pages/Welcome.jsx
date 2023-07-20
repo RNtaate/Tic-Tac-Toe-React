dffb1636d33ec2f3b0049139ddc29853d8f7126b
RNtaate <royhyde14@gmail.com>
2023-07-20 19:46:20 +0300

    Add functionality to display the entered player names on the welcome page.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,10 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import styles from '../styles/Welcome.module.css';
 import PlayerRegForm from '../components/PlayerRegForm';
+import { TicContext } from '../context/TicContextProvider';
 
 const Welcome = () => {
   const [showForm, setShowForm] = useState(false);
+
+  const context = useContext(TicContext);
+  const { players } = context;
+
+  const CreatedPlayerCard = ({ playerName, player }) => {
+    return (
+      <div
+        className={`${styles.chosenPlayerDiv} d-flex flex-col align-items-center justify-content-center`}
+      >
+        <small className={styles.playerCardSmall}>{player}</small>
+        <h4 className={styles.playerCardName}>{playerName}</h4>
+      </div>
+    );
+  };
+
   return (
     <section
       className={`${styles.welcomeMainSection} d-flex justify-content-center align-items-center`}
@@ -21,6 +37,21 @@ const Welcome = () => {
         >
           START
         </button>
+
+        {players.player1 && players.player2 && (
+          <div
+            className={`${styles.chosenPlayersContainer} d-flex justify-content-center align-items-center`}
+          >
+            <CreatedPlayerCard
+              player={'Player 1'}
+              playerName={players.player1}
+            />
+            <CreatedPlayerCard
+              player={'Player 2'}
+              playerName={players.player2}
+            />
+          </div>
+        )}
       </div>
     </section>
   );
