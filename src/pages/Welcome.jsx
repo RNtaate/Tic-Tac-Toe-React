d99506c81c2a5f1fe9889b96aa37631c8b581f42
RNtaate <royhyde14@gmail.com>
2023-07-20 16:58:41 +0300

    Update the design for the welcome page
    Create a style file for the Welcome page
    
    Add styling for the first elements on the welcome page.

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import styles from '../styles/Welcome.module.css';
+
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <section
+      className={`${styles.welcomeMainSection} d-flex justify-content-center align-items-center`}
+    >
+      <div
+        className={`${styles.welcomeContainerDiv} d-flex justify-content-center align-items-center flex-col`}
+      >
+        <h1 className={styles.welcomeHeading}>WELCOME TO NORP'S TIC-TAC-TOE</h1>
+        <button type="button" className={styles.welcomeStartButton}>
+          START
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Welcome;
