ba7bc876e68b3743cd8b88ee2c6bb1919c24758e
RNtaate <royhyde14@gmail.com>
2023-07-20 17:22:15 +0300

    Update the design for the PlayerRefForm component
    Create the associated css file and add some styling.

--- src/components/PlayerRegForm.jsx
+++ src/components/PlayerRegForm.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
+import mainPageStyles from '../styles/MainPage.module.css';
+import styles from '../styles/PlayerRegForm.module.css';
+
 const PlayerRegForm = () => {
-  return <div>PlayerRegForm</div>;
+  return (
+    <section
+      className={`${mainPageStyles.resultSection} ${styles.formBackdropSection} d-flex justify-content-center align-items-center`}
+    >
+      <div className="formContainerDiv">
+        <h3>Please enter the players' names here</h3>
+        <form action="#">
+          <input type="text" placeholder="Player 1's name" name="player1" />
+          <input type="text" placeholder="Player 2's name" name="player2" />
+          <button type="submit">Submit</button>
+          <button type="button">Cancel</button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default PlayerRegForm;
