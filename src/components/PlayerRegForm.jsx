e4020d3725d19344e35206f264ea1c01f18ee92d
RNtaate <royhyde14@gmail.com>
2023-07-20 18:03:34 +0300

    Add the functionality for entering and storing the player names in the context state
    Create the handleSettingPlayerNames function
    
    create local states to get user input.

--- src/components/PlayerRegForm.jsx
+++ src/components/PlayerRegForm.jsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useContext, useRef } from 'react';
 
 import mainPageStyles from '../styles/MainPage.module.css';
 import styles from '../styles/PlayerRegForm.module.css';
+import { TicContext } from '../context/TicContextProvider';
 
 const PlayerRegForm = ({ setShowForm }) => {
+  const [player1Name, setPlayer1Name] = useState('');
+  const [player2Name, setPlayer2Name] = useState('');
+
+  const context = useContext(TicContext);
+  const { setPlayers } = context;
+
+  const handleSettingPlayerNames = (e) => {
+    e.preventDefault();
+    if (!player1Name || !player2Name) return;
+
+    setPlayers({
+      player1: player1Name,
+      player2: player2Name,
+    });
+    setShowForm(false);
+  };
+
   return (
     <section
       className={`${mainPageStyles.resultSection} ${styles.formBackdropSection} d-flex justify-content-center align-items-center`}
@@ -11,9 +29,21 @@ const PlayerRegForm = ({ setShowForm }) => {
     >
       <div className="formContainerDiv" onClick={(e) => e.stopPropagation()}>
         <h3>Please enter the players' names here</h3>
-        <form action="#">
-          <input type="text" placeholder="Player 1's name" name="player1" />
-          <input type="text" placeholder="Player 2's name" name="player2" />
+        <form action="#" onSubmit={handleSettingPlayerNames}>
+          <input
+            type="text"
+            placeholder="Player 1's name"
+            name="player1"
+            autoComplete="off"
+            onChange={(e) => setPlayer1Name(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Player 2's name"
+            name="player2"
+            autoComplete="off"
+            onChange={(e) => setPlayer2Name(e.target.value)}
+          />
           <button type="submit">Submit</button>
           <button type="button" onClick={() => setShowForm(false)}>
             Cancel
