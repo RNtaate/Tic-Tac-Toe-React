8fd8adf4a012e9e58fa4c38e87a2b72d93f1c387
RNtaate <royhyde14@gmail.com>
2023-07-24 13:09:25 +0300

    Update the logo in the navbar to link to the welcome page.
    Update the import statement for the logo image since it is imported from the public folder.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from '../styles/Navbar.module.css';
-import logoSvg from '../../public/tic-icon.svg';
+import logoSvg from '/tic-icon.svg';
 
 const Navbar = () => {
   return (
     <header className={`${styles.navBarHeader} d-flex align-items-center`}>
-      <div className={styles.logoDiv}>
-        <img alt="Company Logo" src={logoSvg} />
-      </div>
-      <h2 className={styles.companyName}>NORP</h2>
+      <Link to="/">
+        <div className={`${styles.logoNameDiv} d-flex align-items-center`}>
+          <div className={styles.logoDiv}>
+            <img alt="Company Logo" src={logoSvg} />
+          </div>
+          <h2 className={styles.companyName}>NORP</h2>
+        </div>
+      </Link>
     </header>
   );
 };
